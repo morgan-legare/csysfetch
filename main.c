1b2fad70c3c92fd0d8e5cf6b9fe0df28b04af7d2
morgan-legare <morganlegare1@yahoo.com>
2025-11-13 16:41:57 -0800

    small update

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main() {
-	
+	char command[250];
+	strcpy(command, "echo $USER");
+	system(command);
+
+	strcpy(command, "lscpu | grep -i \"Model name:\" | awk '{print $3, $5, $6, $7, $8}'");
+	system(command);
+
+	strcpy(command, "lspci | grep -i \"VGA compatible controller: NVIDIA\" | awk '{print $5, $8, $9, $10, $11, $12, $13}'");
+	system(command);
 	return 0;
 }
