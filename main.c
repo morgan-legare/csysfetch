17273c9763e0aef06566e97f69d5e6e5b277ac0e
morgan-legare <morganlegare1@yahoo.com>
2025-11-13 18:33:02 -0800

    another small update

--- main.c
+++ main.c
@@ -4,13 +4,39 @@
 
 int main() {
 	char command[250];
-	strcpy(command, "echo $USER");
+	strcpy(command, "clear");
 	system(command);
 
-	strcpy(command, "lscpu | grep -i \"Model name:\" | awk '{print $3, $5, $6, $7, $8}'");
+	strcpy(command, "echo -------------");
 	system(command);
 
-	strcpy(command, "lspci | grep -i \"VGA compatible controller: NVIDIA\" | awk '{print $5, $8, $9, $10, $11, $12, $13}'");
+	strcpy(command, "echo [ csysfetch ]");
 	system(command);
+
+	strcpy(command, "echo -------------");
+	system(command);
+
+	strcpy(command, "echo");
+	system(command);
+
+	strcpy(command, "arc=$(uname -a | awk '{print $13, $14}') ; . /etc/os-release ; echo OS: $NAME $arc");
+	system(command);
+
+	strcpy(command, "kern=$(uname -a | awk '{print $3}') ; echo Kernel: $kern");
+	system(command);
+
+
+	strcpy(command, "core=$(lscpu | grep -i -m 1 'cpu(s):' | awk '{print $2}') ; cpu=$(lscpu | grep -i \"Model name:\" | awk '{print $3, $5, $6, $7, $8}') ; echo CPU: $cpu [cores: $core]");
+	system(command);
+
+
+	strcpy(command, "gpu1=$(lspci | grep -i \"VGA compatible controller: NVIDIA\" | awk '{print $5, $8, $9, $10, $11, $12, $13}') ; gpu2=$(lspci | grep -i \"VGA compatible controller: Intel\" | awk '{print $5, $9, $10, $11, $12, $13}') ; echo GPU: $gpu1 ; echo GPU: $gpu2");
+	system(command);
+	
+	strcpy(command, "used=$(free -m | awk '/Mem:/ {print $3}') ; total=$(free -m | awk '/Mem:/ {print $2}') ; echo Memory: ${used}MiB / ${total}MiB");
+	system(command);
+	strcpy(command, "echo");
+	system(command);
+
 	return 0;
 }
