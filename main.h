4d32fe16f9e9471bc3b750d6a609477998ec96fa
morgan-legare <morganlegare1@yahoo.com>
2025-11-15 15:40:07 -0800

    update

--- main.h
+++ main.h
@@ -7,7 +7,7 @@
 
 // TO CHANGE COLOR OF LOGO CHANGE THE 95 IN COLOR TO A DIFFERENT ANSI COLOR CODE
 // COMMON ONES ARE: RED = 91, GREEN = 92, YELLOW = 93, BLUE = 94, PURPLE = 95, CYAN = 96, WHITE 97
-const char* COLOR = "\033[0;95m";
+const char* COLOR = "\033[0;92m";
 const char* RESET = "\033[m";
 
 
@@ -27,7 +27,7 @@ int displayInfo() {
 	}
 	char buff[100];
 	while (fgets(buff, sizeof(buff), fptr) != NULL) {
-		printf("%s", buff);
+		printf("  %s", buff, "%s");
 	}
 
 	fclose(fptr);
