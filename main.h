80529941d20a2c1167c46e3989eed083877b1da3
morgan-legare <morganlegare1@yahoo.com>
2025-11-15 20:49:20 -0800

    added ability to easily change display colors

--- main.h
+++ main.h
@@ -4,20 +4,27 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
-// TO CHANGE COLOR OF LOGO CHANGE THE 95 IN COLOR TO A DIFFERENT ANSI COLOR CODE
-// COMMON ONES ARE: RED = 91, GREEN = 92, YELLOW = 93, BLUE = 94, PURPLE = 95, CYAN = 96, WHITE 97
-const char* COLOR = "\033[0;97m";
 const char* RESET = "\033[m";
 
+// COLOR OPTIONS FOR LOGO/SYSTEM INFO
+#define RED "\033[0;91m";
+#define GREEN "\033[0;92m";
+#define YELLOW "\033[0;93m";
+#define BLUE "\033[0;94m";
+#define PURPLE "\033[0;95m";
+#define CYAN "\033[0;96m";
+#define DEFAULT_COLOR "\033[0;97m";
+
+// TO CHANGE COLOR OF LOGO/SYSTEM INFO, CHANGE DEFAULT_COLOR TO ONE OF THE ABOVE COLORS 
+const char* LOGO_COLOR = DEFAULT_COLOR;
+const char* INFO_COLOR = DEFAULT_COLOR; 
 
 // PRINTS THE COLORED LOGO 
 void printLogo() { 
-	printf("%s\n=============\n%s",COLOR, RESET);
-	printf("%s[ csysfetch ]\n%s", COLOR, RESET);
-	printf("%s=============\n\n%s", COLOR, RESET);
+	printf("%s\n=============\n%s", LOGO_COLOR, RESET);
+	printf("%s[ csysfetch ]\n%s", LOGO_COLOR, RESET);
+	printf("%s=============\n\n%s", LOGO_COLOR, RESET);
 }
-
 
 // DISPLAYS THE SYSTEM INFO
 int displayInfo() {
@@ -27,7 +34,8 @@ int displayInfo() {
 	}
 	char buff[100];
 	while (fgets(buff, sizeof(buff), fptr) != NULL) {
-		printf("  %s", buff, "%s");
+		printf("%s  %s%s", INFO_COLOR, buff, RESET);
+		
 	}
 
 	fclose(fptr);
